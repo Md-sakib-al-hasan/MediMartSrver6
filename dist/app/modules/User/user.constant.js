26884cad3480438848fd633692b96d12ddc0d09e
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-04-15 21:57:26 +0600

    uk

--- dist/app/modules/User/user.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserGender = exports.UserSearchableFields = void 0;
-exports.UserSearchableFields = ['email', 'name', 'role'];
-exports.UserGender = ['Male', 'Female', 'Other'];
