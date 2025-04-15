eadfeff77b9a38f3e1dea666409f1875d6e153c3
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-04-15 21:26:42 +0600

    sk

--- src/app/modules/meta/meta.route.ts
+++ src/app/modules/meta/meta.route.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { MetaController } from './meta.controller';
+import auth from '../../middleware/auth';
+import { UserRole } from '../user/user.interface';
 
 const router = Router();
 
-router.get(
-  '/',
-  // auth(UserRole.ADMIN),
-  MetaController.getMetaData,
-);
+router.get('/', auth(UserRole.ADMIN), MetaController.getMetaData);
 
 export const MetaRoutes = router;
